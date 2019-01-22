afc39b48473dfe49e15476302282f92dc8200dfb
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-22 11:19:59 -0700

    adding state and reorganizing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ function Header() {
 }
 
 class BestSellers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleDateString()
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="date">{this.state.date}</div>
+        <BestSellersTable date={this.state.date} />
+      </div>
+    );
+  }
+}
+
+class BestSellersTable extends React.Component {
   render() {
     return(
       <table>
@@ -19,22 +37,9 @@ class BestSellers extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <BestSellersRow />
+          <BestSeller />
         </tbody>
       </table>
-    );
-  }
-}
-
-class BestSellersRow extends React.Component {
-  render() {
-    return(
-      <tr>
-        <td>1</td>
-        <td><BestSeller /></td>
-        <td>1</td>
-        <td>10</td>
-      </tr>
     );
   }
 }
@@ -42,11 +47,18 @@ class BestSellersRow extends React.Component {
 class BestSeller extends React.Component {
   render() {
     return(
-      <div>
-        <div className="title">AN ANONYMOUS GIRL</div>
-        <div className="author">by Greer Hendricks and Sarah Pekkanen</div>
-        <div className="description">Jessica Farris’s life unravels when she signs up for Dr. Shields’s psychology study.</div>
-      </div>
+      <tr>
+        <td>1</td>
+        <td>
+          <div>
+            <div className="title">AN ANONYMOUS GIRL</div>
+            <div className="author">by Greer Hendricks and Sarah Pekkanen</div>
+            <div className="description">Jessica Farris’s life unravels when she signs up for Dr. Shields’s psychology study.</div>
+          </div>
+        </td>
+        <td>1</td>
+        <td>10</td>
+      </tr>
     );
   }
 }
