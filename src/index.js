f0c71538c313d446f679e86dea4a0207b267ac74
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-22 12:09:14 -0700

    fetching NY Times API

--- src/index.js
+++ src/index.js
@@ -25,22 +25,51 @@ class BestSellers extends React.Component {
 }
 
 class BestSellersTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=' + process.env.REACT_APP_NYTIMES_KEY)
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            books: result.results
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
   render() {
-    return(
-      <table>
-        <thead>
-          <tr>
-            <th>Ranking</th>
-            <th>Book</th>
-            <th>Last Week’s Rank</th>
-            <th>Weeks on List</th>
-          </tr>
-        </thead>
-        <tbody>
-          <BestSeller />
-        </tbody>
-      </table>
-    );
+    if (this.state.isLoaded) {
+      return(
+        <table>
+          <thead>
+            <tr>
+              <th>Ranking</th>
+              <th>Book</th>
+              <th>Last Week’s Rank</th>
+              <th>Weeks on List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <BestSeller books={this.state.books} />
+          </tbody>
+        </table>
+      );
+    } else {
+      return(
+        <div className="loading">loading...</div>
+      );
+    }
   }
 }
 
