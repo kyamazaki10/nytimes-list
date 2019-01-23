2d7caaafe6a9a743f8a0629e9dc1ca0919f14d66
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-23 07:38:42 -0700

    adding Amazon link

--- src/BestSellersTableRow.js
+++ src/BestSellersTableRow.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Link from '@material-ui/core/Link';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 import './BestSellersTableRow.css';
 
 class BestSellersTableRow extends React.Component {
@@ -15,9 +17,11 @@ class BestSellersTableRow extends React.Component {
         <TableRow hover key={i}>
           <TableCell>{i + 1}</TableCell>
           <TableCell>
-            <div className="title">{details.title}</div>
-            <div className="author">by {details.author}</div>
-            <div className="description">{details.description}</div>
+            <Typography variant="subtitle1">
+              <Link href={book.amazon_product_url}>{details.title}</Link>
+            </Typography>
+            <Typography variant="subtitle2" className="author">by {details.author}</Typography>
+            <Typography variant="caption" className="description">{details.description}</Typography>
           </TableCell>
           <TableCell>{book.rank_last_week}</TableCell>
           <TableCell>{book.weeks_on_list}</TableCell>
