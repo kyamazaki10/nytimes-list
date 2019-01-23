7b6945ea635d920932b54d8fb289f95af3f13adc
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-23 12:01:50 -0700

    adding AppBar to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import './Header.css';
 
 function Header() {
   return (
-    <Typography component="h1" variant="h2" gutterBottom>
-      The New York Times Best Sellers List
-    </Typography>
+    <AppBar color="primary">
+      <Toolbar>
+        <IconButton aria-label="Menu" className="icon">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className="category">
+          Hardcover Fiction
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
