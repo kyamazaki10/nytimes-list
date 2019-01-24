9d48585c91e4c862cf538a5157b6d5093a4de6e3
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-24 12:34:35 -0700

    moving categories to separate JSON file

--- src/Header/HeaderMenu.js
+++ src/Header/HeaderMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import categories from './Categories.json';
 
 class HeaderMenu extends React.Component {
   constructor(props) {
@@ -18,69 +19,8 @@ class HeaderMenu extends React.Component {
     this.props.onClick(e.currentTarget.id);
   }
 
-  getSubcategories(category) {
-    const subcategories = {
-      nonfiction: [
-        {
-          name: 'Combined Print and E-Book Nonfiction',
-          id: 'combined-print-and-e-book-nonfiction'
-        },
-        {
-          name: 'Hardcover Nonfiction',
-          id: 'hardcover-nonfiction'
-        },
-        {
-          name: 'Paperback Nonfiction',
-          id: 'paperback-nonfiction'
-        },
-        {
-          name: 'Advice, How-To & Miscellaneous',
-          id: 'advice-how-to-and-miscellaneous'
-        }
-      ],
-      fiction: [
-        {
-          name: 'Combined Print & E-Book Fiction',
-          id: 'combined-print-and-e-book-fiction'
-        },
-        {
-          name: 'Hardcover Fiction',
-          id: 'hardcover-fiction'
-        },
-        {
-          name: 'Trade Paperback Fiction',
-          id: 'trade-fiction-paperback'
-        },
-        {
-          name: 'Paperback Fiction',
-          id: 'paperback-fiction'
-        }
-      ],
-      children: [
-        {
-          name: 'Children’s Middle Grade Hardcover',
-          id: 'childrens-middle-grade-hardcover'
-        },
-        {
-          name: 'Picture Books',
-          id: 'picture-books'
-        },
-        {
-          name: 'Series Books',
-          id: 'series-books'
-        },
-        {
-          name: 'Young Adult Hardcover',
-          id: 'young-adult-hardcover'
-        }
-      ]
-    };
-
-    return subcategories[category];
-  }
-
   renderMenuItem(category) {
-    const subcategories = this.getSubcategories(category);
+    const subcategories = categories[category];
     let items = [];
 
     for (let subcategory of subcategories) {
