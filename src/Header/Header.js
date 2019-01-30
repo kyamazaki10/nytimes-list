0f275861954f36b89b79b21f8826b7e21fcca0e5
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-01-30 13:28:10 -0700

    hopefully some nice Header optimizations

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,42 +1,18 @@
 import React from 'react';
 import HeaderMenu from './HeaderMenu.js';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import './Header.css';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nonfiction: null,
-      fiction: null,
-      children: null
-    }
 
-    this.handleCategoryClick = this.handleCategoryClick.bind(this);
     this.handleSubcategoryClick = this.handleSubcategoryClick.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-
-  handleCategoryClick(e) {
-    this.setState({
-      [e.currentTarget.id]: e.currentTarget
-    });
   }
 
   handleSubcategoryClick(id) {
     this.props.onClick(id);
-  }
-
-  handleClose() {
-    for (let key in this.state) {
-      if (this.state[key] !== null) {
-        this.setState({
-          [key]: null
-        });
-      }
-    }
   }
 
   renderMenu() {
@@ -45,18 +21,10 @@ class Header extends React.Component {
       { id: 'fiction', name: 'Fiction' },
       { id: 'children', name: 'Children’s' }
     ];
-    let menu = [];
 
-    for (let category of categories) {
-      menu.push(
-        <div key={category.id}>
-          <Button id={category.id} onClick={this.handleCategoryClick} aria-label={category.name} className="menu-button">{category.name}</Button>
-          <HeaderMenu id={category.id} anchorEl={this.state[category.id]} onClick={this.handleSubcategoryClick} onClose={this.handleClose} />
-        </div>
-      );
-    }
-
-    return menu;
+    return categories.map((category) =>
+      <HeaderMenu key={category.id} id={category.id} name={category.name} onClick={this.handleSubcategoryClick} />
+    );
   }
 
   render() {
--- src/Header/HeaderMenu.js
+++ src/Header/HeaderMenu.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import categories from './Categories.json';
@@ -6,35 +7,48 @@ import categories from './Categories.json';
 class HeaderMenu extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      anchorEl: null
+    };
 
     this.handleClose = this.handleClose.bind(this);
+    this.handleCategoryClick = this.handleCategoryClick.bind(this);
     this.handleSubcategoryClick = this.handleSubcategoryClick.bind(this);
   }
 
   handleClose() {
-    this.props.onClose();
+    this.setState({
+      anchorEl: null
+    });
+  }
+
+  handleCategoryClick(e) {
+    this.setState({
+      anchorEl: e.currentTarget
+    });
   }
 
   handleSubcategoryClick(e) {
     this.props.onClick(e.currentTarget.id);
+    this.handleClose();
   }
 
   renderMenuItem(category) {
     const subcategories = categories[category];
-    let items = [];
 
-    for (let subcategory of subcategories) {
-      items.push(<MenuItem key={subcategory.id} id={subcategory.id} onClick={this.handleSubcategoryClick}>{subcategory.name}</MenuItem>);
-    }
-
-    return items;
+    return subcategories.map((subcategory) =>
+      <MenuItem key={subcategory.id} id={subcategory.id} onClick={this.handleSubcategoryClick}>{subcategory.name}</MenuItem>
+    );
   }
 
   render() {
     return(
-      <Menu id={this.props.id} anchorEl={this.props.anchorEl} open={Boolean(this.props.anchorEl)} onClose={this.handleClose}>
-        {this.renderMenuItem(this.props.id)}
-      </Menu>
+      <Fragment>
+        <Button aria-label={this.props.name} onClick={this.handleCategoryClick} className="menu-button">{this.props.name}</Button>
+        <Menu id={this.props.id} anchorEl={this.state.anchorEl} open={Boolean(this.state.anchorEl)} onClose={this.handleClose}>
+          {this.renderMenuItem(this.props.id)}
+        </Menu>
+      </Fragment>
     );
   }
 }
