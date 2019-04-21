529e9759892443ed6d4bf10d69594100790e67bd
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-04-21 14:13:41 -0600

    Updates

---
+++ precache-manifest.3a0832914dec0fdddbfb77cf254eb37d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90bcc79e439ef723deef",
+    "url": "/nytimes-list/static/js/runtime~main.90bcc79e.js"
+  },
+  {
+    "revision": "e5317aec18aeec52ebf6",
+    "url": "/nytimes-list/static/js/main.e5317aec.chunk.js"
+  },
+  {
+    "revision": "7ea1a5308178058918e6",
+    "url": "/nytimes-list/static/js/1.7ea1a530.chunk.js"
+  },
+  {
+    "revision": "e5317aec18aeec52ebf6",
+    "url": "/nytimes-list/static/css/main.dd2cb8a4.chunk.css"
+  },
+  {
+    "revision": "670f34e5662cc2377805227dde111901",
+    "url": "/nytimes-list/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kyamazaki10/nytimes-list/precache-manifest.e635a374bcd24f492be1eb5ad1f0827c.js"
+  "/nytimes-list/precache-manifest.3a0832914dec0fdddbfb77cf254eb37d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kyamazaki10/nytimes-list/index.html", {
+workbox.routing.registerNavigationRoute("/nytimes-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
