26377eb8f1ef459d034755efd78f04b7b924be8c
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-04-21 13:17:19 -0600

    Updates

---
+++ precache-manifest.e635a374bcd24f492be1eb5ad1f0827c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8977f659007b9ff2b7af",
+    "url": "/kyamazaki10/nytimes-list/static/js/runtime~main.8977f659.js"
+  },
+  {
+    "revision": "655c97167c78a3ee39db",
+    "url": "/kyamazaki10/nytimes-list/static/js/main.655c9716.chunk.js"
+  },
+  {
+    "revision": "7ea1a5308178058918e6",
+    "url": "/kyamazaki10/nytimes-list/static/js/1.7ea1a530.chunk.js"
+  },
+  {
+    "revision": "655c97167c78a3ee39db",
+    "url": "/kyamazaki10/nytimes-list/static/css/main.dd2cb8a4.chunk.css"
+  },
+  {
+    "revision": "6379a73f0272b8f46ca8c86e3421bfa8",
+    "url": "/kyamazaki10/nytimes-list/index.html"
+  }
+];
