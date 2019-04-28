90676390e85a798b13352c4fd72ff2cee565c8b7
Kumiko Yamazaki <kyamazaki@gmail.com>
2019-04-28 13:03:43 -0600

    clearing up warning for missing alt attribute

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,9 @@ function Footer() {
   return (
     <div className="footer">
       <a href="https://developer.nytimes.com/">
-        <img src="https://developer.nytimes.com/files/poweredby_nytimes_200a.png?v=1539041465000" />
+        <img
+          src="https://developer.nytimes.com/files/poweredby_nytimes_200a.png?v=1539041465000"
+          alt="Data provided by The New York Times" />
       </a>
     </div>
   );
